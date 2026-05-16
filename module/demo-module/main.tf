e06211fe8a9a27f2984634c175b10078ffc75554
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-05-16 20:32:49 +0400

    Modules learn created

---
+++ module/demo-module/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+resource "null_resource" "test1" {
+}
+resource "null_resource" "test2" {
+}
+resource "null_resource" "test3" {
+}
+resource "null_resource" "test4" {
+}
